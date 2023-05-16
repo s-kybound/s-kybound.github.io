2bd33d2ecd0c443ab8448ad738decb06d2eedbf9
Kyriel Abad <abadkyriel@gmail.com>
2023-05-16 18:48:20 +0800

    added base react

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          New website is in progress! -s-kybound
+          Now using CHAD react instead of raw HTML
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
