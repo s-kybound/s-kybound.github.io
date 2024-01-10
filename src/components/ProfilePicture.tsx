b9dea4439a9b5c690b1b4ca461721809182699e6
Kyriel Abad <abadkyriel@gmail.com>
2024-01-11 01:19:12 +0800

    Update github workflow

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -2,9 +2,12 @@ import kyriel from "../../public/Kyriel_Linkedin.png";
 import "./ProfilePicture.css";
 
 export default function ProfilePicture() {
-    return (
-        <>
-            <div className="profile-picture" style={{ backgroundImage: `url(${kyriel})` }}></div>
-        </>
-    );
+  return (
+    <>
+      <div
+        className="profile-picture"
+        style={{ backgroundImage: `url(${kyriel})` }}
+      ></div>
+    </>
+  );
 }
